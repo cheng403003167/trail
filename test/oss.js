ee6991b09f86224eb890a27cf259824808534416
cheng403003167 <403003167@qq.com>
2018-04-06 14:36:32 +0800

    添加事件模型代码示例

--- test/oss.js
+++ test/oss.js
@@ -1,37 +1,30 @@
 var co = require('co');
 var OSS = require('ali-oss');
 var fs = require('fs');
+const chalk = require('chalk');
 var writeFile = require('./getData');
 
 
-var url = 'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2457983084.jpg';
+// var url = 'https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2457983084.jpg';
 var client = new OSS({
   accessKeyId: 'LTAIOs2a0NczRHfB',
   accessKeySecret: 'Tl9ohhSUrFIFyRHmrBSMAryzjJWOzV',
   bucket:'traile',
   endpoint:'oss-cn-shenzhen.aliyuncs.com'
 });
-var Data = '';
-// http.get(url,(res)=>{
-//   res.setEncoding('binary');
-//   res.on('data',(d)=>{
-//       Data += d;
-//   });
-//   res.on('end',()=>{
-//     db.write(Data,'binary');
-//     db.pipe(fs.createWriteStream('ab.mp4'))
-//   })
-// });
-co(function* () {
-  yield writeFile.downHttpData(url,'binary').then(function(stra){
-    Data = stra;
+function upDate(url){
+  var Data = '';
+  co(function* () {
+    yield writeFile.downHttpData(url,'binary').then(function(stra){
+      Data = stra;
+    });
+    var result = yield client.putStream(Data.fileName,Data.duplex);
+    console.log(chalk.green(Data.fileName + '上传成功'))
+  }).catch(function (err) {
+    console.error(chalk.blue(Data.fileName + '上传失败' + err));
   });
-  var result = yield client.putStream(Data.fileName,Data.duplex);
-  console.log(result.name);
-  
-}).catch(function (err) {
-  console.error(err);
-});
+}
+module.exports = upDate;
 
 
 
