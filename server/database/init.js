a2fb4c4811bd2419b428f7283de25a285bbc39ac
cheng403003167 <403003167@qq.com>
2018-04-07 21:40:38 +0800

    加入数据库代码

---
+++ server/database/init.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const db = 'mongodb://localhost/demo';
+
+
+mongoose.Promise = global.Promise;
+
+module.exports = connect = () =>{
+    let maxConnectTimes = 0;
+
+    return new Promise((resolve,reject)=>{
+        if(process.env.NODE_ENV !== 'production'){
+            mongoose.set('debug',true);
+        }
+        mongoose.connect(db);
+        mongoose.connection.on('disconnected',()=>{
+            maxConnectTimes++;
+            if(maxConnectTimes < 5){
+                mongoose.connect(db);
+            }else{
+                throw new Error('数据库挂了')
+            }
+        })
+        mongoose.connection.on('error',err=>{
+            maxConnectTimes++;
+            if(maxConnectTimes < 5){
+                mongoose.connect(db);
+            }else{
+                throw new Error('数据库挂了')
+            }
+        })
+        mongoose.connection.once('open',()=>{
+            resolve();
+            console.log('MongoDB Connected successed')
+        })
+    })
+}
--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const Koa = require('koa2');
-const app = new Koa();
 const views = require('koa-views');
-const { resolve } = require('path')
+const { resolve } = require('path');
+const connect= require('./database/init');
 
+;(async ()=>{
+    await connect();
+})()
+
+const app = new Koa();
 
 app.use(views(resolve(__dirname,'./views'),{
     extension: 'pug'
